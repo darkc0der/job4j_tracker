f5b4ffe32361342a0b7278421f5eeaa51da36975
ADMIN <borisroud1@mail.ru>
2021-04-06 08:15:35 +0800

    1.3. Вызов метода с аргументами. [#393904]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,8 +2,7 @@ package ru.job4j.oop;
 
 public class Cat {
     public String sound() {
-        String voice = "may-may";
-        return voice;
+        return "may-may";
     }
 
     public static void main(String[] args) {
@@ -12,5 +11,8 @@ public class Cat {
         Cat sparky = new Cat();
         String say = peppy.sound();
         System.out.println("Peppy says " + say);
+
+        say = sparky.sound();
+        System.out.println("Sparky says " + say);
     }
 }
