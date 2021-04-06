93b702a618caf3720c1190c9a446ed09fa9d9649
ADMIN <borisroud1@mail.ru>
2021-04-06 08:19:32 +0800

    1.3. Вызов метода с аргументами. [#393904]

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+/* 1.4. Вызов метода с возвращаемым типом. [#393906] */
+public class DummyDic {
+    public String engToRus(String eng) {
+        return "Неизвестное слово " + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic dd = new DummyDic();
+        String word = dd.engToRus("Hello world!");
+        System.out.println(word);
+    }
+}
