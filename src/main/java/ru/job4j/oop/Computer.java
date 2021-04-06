f76b7213d3bc717f313b1c6378a3eb01d5fee161
ADMIN <borisroud1@mail.ru>
2021-04-06 08:13:37 +0800

    1.3. Вызов метода с аргументами. [#393904]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -1,15 +1,13 @@
 package ru.job4j.oop;
 
 public class Computer {
+
     private boolean multiMonitor;
     private int ssd;
     private String cpu;
 
-    public void printInfo() {
-        System.out.println("Много мониторов: " + multiMonitor);
-        System.out.println("SSD: " + ssd + " GB");
-        System.out.println("Модель CPU: " + cpu);
-        System.out.println();
+    public Computer() {
+
     }
 
     public Computer(boolean multiMonitor, int ssd, String cpu) {
@@ -18,17 +16,18 @@ public class Computer {
         this.cpu = cpu;
     }
 
-    public Computer() {
-
+    public void printInfo() {
+        System.out.println("Много мониторов: " + multiMonitor);
+        System.out.println("SSD: " + ssd + " GB");
+        System.out.println("Модель CPU: " + cpu);
+        System.out.println();
     }
 
     public static void main(String[] args) {
         Computer intel = new Computer(true, 500, "Intel Core I7-10700K");
         intel.printInfo();
-
         Computer amd = new Computer(true, 256, "AMD Ryzen 5 3600");
         amd.printInfo();
-
         Computer defaultComputer = new Computer();
         defaultComputer.printInfo();
     }
