27729b056a6074270fe356ecba5374eb40519dcb
ADMIN <borisroud1@mail.ru>
2021-05-17 20:57:47 +0800

    3. Банковские переводы [#393839]

---
+++ src/main/java/ru/job4j/collection/map/banktransfers/BankService.java
@@ -0,0 +1,56 @@
+package ru.job4j.collection.map.banktransfers;
+/* 3. Банковские переводы [#393839] */
+import java.util.*;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) {
+                accounts.add(account);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (Account account : users.get(user)) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPass, String srcReq,
+                                 String destPass, String destReq, double amount) {
+        boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPass, srcReq);
+        Account destAccount = findByRequisite(destPass, destReq);
+
+        if (srcAccount != null && destAccount != null && srcAccount.getBalance() >= amount) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
