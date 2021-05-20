96967de460d061efe9b8dd95f310b2598b855b7c
ADMIN <borisroud1@mail.ru>
2021-05-20 22:31:22 +0800

    4. Сортировка номера [#393824]

---
+++ src/main/java/ru/job4j/collection/sort/lexsort/LexSort.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection.sort.lexsort;
+
+import java.util.Comparator;
+import java.util.regex.Pattern;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String first, String second) { // "1. test
+        String[] left = first.split(Pattern.quote(".")); // можно так
+        String[] right = second.split("\\."); // а можно вот так
+        return Integer.compare(Integer.parseInt(left[0]), Integer.parseInt(right[0]));
+    }
+}
